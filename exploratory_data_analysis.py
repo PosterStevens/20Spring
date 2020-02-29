4a06c4822beb47f546d40e64ced279c8c47f2674
F820914768 <820914768@qq.com>
2020-02-29 17:24:32 -0500

    add basic click interval analysis and missing value analysis

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -4,14 +4,95 @@ import utils
 import dataset.load_dataset as data_load
 import dataset.select_data as data_select
 import pandas as pd
+import numpy as np
 
+import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 
 if __name__ == '__main__':
     print(data_load.load_click.__doc__)
     df_click = data_load.load_click()
+    df_click['click_interval'] = df_click.request_time.diff(1)
+
     
-    d = data_select.select_timerange(df_click, '2018-2-1',
-                                     '2018-4-1')
+    
+    plt.figure(figsize=(14,12))
+    df_click.request_time.hist(bins=40)
+    plt.xlabel('date')
+    plt.ylabel('number of click')
+    plt.title('histogram of click')
+#    plt.savefig('figure/EDA/histogram_click.png')
+    
+    
+    
+# =============================================================================
+#     Analyze missing value
+# =============================================================================
+
+    df_user_missing = df_click.query('user_ID == "-"')
+    plt.figure(figsize=(14,12))
+    df_user_missing.request_time.hist(bins=40)
+    plt.xlabel('date')
+    plt.ylabel('number of click where user is missing')
+    plt.title('histogram of missing user')
+    plt.savefig('figure/EDA/histogram_click_usermissing.png')
+    
+    
+    
+    df_user_notmissing = df_click.query('user_ID != "-"')
+    plt.figure(figsize=(14,12))
+    df_user_notmissing.request_time.hist(bins=40)
+    plt.xlabel('date')
+    plt.ylabel('number of click where user is missing')
+    plt.title('histogram of non missing user')
+    plt.savefig('figure/EDA/histogram_click_nonmissing.png')
+    
+    
+# =============================================================================
+#     Analyze click interval
+# =============================================================================
+    
+    same_user_indicator = df_user_notmissing.user_ID.shift(1) == df_user_notmissing.user_ID
+    click_interval = df_user_notmissing['click_inverval'][same_user_indicator]
+    click_interval = click_interval.apply(lambda x: x.seconds)
+    
     
 
+    click_interval[click_interval<200].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/0_200.png')
+    
+    click_interval[click_interval<20].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/0_20.png')
+    
+    click_interval[click_interval<1280].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/0_1280.png')
+    
+    click_interval[(click_interval<1280)&(click_interval>640)].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/640_1280.png')
+    
+    click_interval[(click_interval<1280)&(click_interval>640)].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/320_640.png')
+    
+    click_interval[(click_interval<2560)&(click_interval>1280)].hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/1280_2560.png')
+    
+    click_interval.hist(bins=40)
+    plt.xlabel('wait_between_clicks')
+    plt.ylabel('number')
+    plt.savefig('figure/EDA/click_interval/0_all.png')
+    
+    utils.save_pickle(click_interval, 'click_interval.pk')
+    
