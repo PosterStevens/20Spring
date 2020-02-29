de68fcb99d50616a49b35a126e0b4482e5572f25
F820914768 <820914768@qq.com>
2020-02-29 01:42:53 -0500

    add str2date function

--- utils.py
+++ utils.py
@@ -24,3 +24,16 @@ def save_pickle(obj, path):
     with open(path, 'wb') as f:
         dump(obj, path)
     return None
+
+
+def str2date(s):
+    '''
+    convert str into pandas.datetime
+    
+    >>> str2date('2019-1-1')
+    >>> datetime.datetime(2019, 1, 1, 0, 0)
+    '''
+    from pandas import datetime
+    date_list = s.strip().split('-')
+    date_list = [int(x) for x in date_list]
+    return datetime(*date_list)
