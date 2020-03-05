75873e5bc6841bfddb0116ac26956c68df1d463d
F820914768 <820914768@qq.com>
2020-03-05 18:36:41 -0500

    add sort

--- dataset/join_dataset.py
+++ dataset/join_dataset.py
@@ -4,7 +4,8 @@ import load_dataset
 
 
 def join_click_order(cols1 = ['user_ID', 'sku_ID', 'request_date', 'request_time'],
-                     cols2 = ['user_ID', 'sku_ID', 'order_date', 'order_ID']):
+                     cols2 = ['user_ID', 'sku_ID', 'order_date', 'order_ID'],
+                     sort = ['user_ID', 'request_time']):
 
     click_table = load_dataset.load_click(frequency='d',
                                           sort=None)[cols1]
@@ -14,6 +15,8 @@ def join_click_order(cols1 = ['user_ID', 'sku_ID', 'request_date', 'request_time
                       left_on = ['user_ID', 'sku_ID', 'request_date'],
                       right_on = ['user_ID', 'sku_ID', 'order_date'])
     df['if_order'] =  ~df.order_ID.isnull()
+    if sort:
+        df.sort_values(sort, inplace=True)
     return df
     
 
