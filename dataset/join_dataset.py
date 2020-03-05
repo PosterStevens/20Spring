5b0bb5992491d90ff254e90fbe3858342c16955c
F820914768 <820914768@qq.com>
2020-03-05 18:19:38 -0500

    add table join

--- dataset/join_dataset.py
+++ dataset/join_dataset.py
@@ -6,14 +6,15 @@ import load_dataset
 def join_click_order(cols1 = ['user_ID', 'sku_ID', 'request_date', 'request_time'],
                      cols2 = ['user_ID', 'sku_ID', 'order_date', 'order_ID']):
 
-    click_table = load_dataset.load_click(frequency='d')[cols1]
+    click_table = load_dataset.load_click(frequency='d',
+                                          sort=None)[cols1]
     order_table = load_dataset.load_order()[cols2]
 
-    click_table.merge(order_table, how='outer',
+    df = click_table.merge(order_table, how='outer',
                       left_on = ['user_ID', 'sku_ID', 'request_date'],
                       right_on = ['user_ID', 'sku_ID', 'order_date'])
-    
-    return click_table
+    df = df[~df.order_ID.isnull()]
+    return df
     
 
 
