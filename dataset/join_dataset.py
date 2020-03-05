bc07e55da187ee90afec3ae6cfa442fed67be1b7
F820914768 <820914768@qq.com>
2020-03-05 17:26:39 -0500

    to join tables

--- dataset/join_dataset.py
+++ dataset/join_dataset.py
@@ -3,11 +3,17 @@ import load_dataset
 
 
 
-def join_click_order():
-    click_table = load_dataset.load_click()
-    order_table = load_dataset.load_order()
+def join_click_order(cols1 = ['user_ID', 'sku_ID', 'request_date'],
+                     cols2 = ['user_ID', 'sku_ID', 'order_date', 'order_ID']):
+    from pandas import merge
+    click_table = load_dataset.load_click()[cols1]
+    order_table = load_dataset.load_order()[cols2]
+
+    click_table.merge(order_table, how='outer',
+                      left_on = ['user_ID', 'sku_ID', 'request_date'],
+                      right_on = ['user_ID', 'sku_ID', 'order_date'])
     
-    
+    return click_table
     
 
 
