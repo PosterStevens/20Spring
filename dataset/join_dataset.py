273212a5db6478d205e7e9bb1391d86fcbccd964
F820914768 <820914768@qq.com>
2020-03-05 16:22:06 -0500

    join tables

---
+++ dataset/join_dataset.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import load_dataset
+
+
+
+def join_click_order():
+    click_table = load_dataset.load_click()
+    order_table = load_dataset.load_order()
+    
+    
+    
+
+
+
+
