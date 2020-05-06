324ee538c309957cfdc21879854e10f1df2dac37
f820914768 <820914768@qq.com>
2020-05-06 22:42:47 +0800

    run pipeline of data preprocessing

---
+++ data_preprocessing.bat
@@ -0,0 +1,5 @@
+python join_table.py
+cd ./data
+data_preprocessing.exe 2 click_order_user.csv user_n_gram.csv user_level gender education city_level purchase_power martial_status if_order
+data_preprocessing.exe 2 click_order_sku.csv sku_n_gram.csv sku_ID 
+cd ..
