2f09ef67b3d0c1d73f3337f73ac2697b69761b23
f820914768 <820914768@qq.com>
2020-05-06 22:54:50 +0800

    pipeline of data preprocessing

--- data_preprocessing.bat
+++ data_preprocessing.bat
@@ -3,19 +3,3 @@ cd ./data
 data_preprocessing.exe 2 click_order_user.csv user_n_gram.csv user_level gender education city_level purchase_power martial_status if_order
 data_preprocessing.exe 2 click_order_sku.csv sku_n_gram.csv sku_ID if_order
 cd ..
-
-
-
-
-
-
-
-
-
-
-
-
-
-a_1, a_0, b_0, a_0, c_0
-
-b	a, a, a, c	c	
