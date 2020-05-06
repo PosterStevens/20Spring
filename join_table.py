87e355268a388caf49a3d8449a6aac6e99ef8684
f820914768 <820914768@qq.com>
2020-05-06 22:41:32 +0800

    join click table with other tables, sort by userID and date then write into csv file

---
+++ join_table.py
@@ -0,0 +1,31 @@
+from dataset.load_dataset import load_click, load_order, load_sku, load_user
+from dataset.join_dataset import join_click_order
+from dataset.select_data import select_timerange
+from dataset.transform import to_sequence
+import pandas as pd
+
+
+
+
+if __name__ == "__main__":
+    df_click_order = join_click_order()
+    df_sku = load_sku()
+
+    print('begin to merge click table and sku table')
+    df_click_order_sku = df_click_order.merge(df_sku, left_on='sku_ID',
+                                                right_on = 'sku_ID')
+    print('write data into click_order_sku table')
+    df_click_order_sku = df_click_order_sku.query('user_ID != "-"')    
+    
+
+    df_click_order_sku.to_csv('./data/click_order_sku.csv')
+
+    del df_click_order_sku, df_sku
+
+
+    df_user = load_user()
+    print('begin to merge click table and user table')
+    df_click_order_user = df_click_order.merge(df_user, left_on='user_ID',
+                                                    right_on='user_ID')
+    print('write data into click_order_user table')
+    df_click_order_user.to_csv('./data/click_order_user.csv')
