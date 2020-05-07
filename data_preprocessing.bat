22a1ba07fff4a5fe456946702ae8aed1533e88cb
f820914768 <820914768@qq.com>
2020-05-08 01:52:14 +0800

    run join_table, then run ./data/preprocessing.exe

--- data_preprocessing.bat
+++ data_preprocessing.bat
@@ -1,5 +1,5 @@
 python join_table.py
 cd ./data
-data_preprocessing.exe 2 click_order_user.csv user_n_gram.csv user_level gender education city_level purchase_power martial_status if_order
-data_preprocessing.exe 2 click_order_sku.csv sku_n_gram.csv sku_ID if_order
+data_preprocessing.exe 2 sku_ID click_order_user.csv user_n_gram.csv user_level gender education city_level purchase_power marital_status age
+data_preprocessing.exe 2 user_ID click_order_sku.csv sku_n_gram.csv sku_ID if_order
 cd ..
